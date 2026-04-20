d38609f124a12708c03029d50cffe89864aa59e7
phessophissy <73696997+phessophissy@users.noreply.github.com>
2026-04-20 08:34:56 +0100

    chore: export new utility modules from barrel file

--- src/exports.ts
+++ src/exports.ts
@@ -6,3 +6,10 @@ export * from './transaction';
 export * from './pagination';
 export * from './filters';
 export * from './format';
+
+export * from './validation';
+export * from './retry';
+export * from './cache';
+export * from './logger';
+export * from './version';
+export * from './gas';
